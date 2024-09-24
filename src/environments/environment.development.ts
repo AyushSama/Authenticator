5ca7cc29735b0dae664f84648b7f3ca334254cc2
AyushSama <aayush0029@gmail.com>
2024-09-24 19:55:54 +0530

    Edited Keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseApiUrl : "https://localhost:44316/api/UserAuthenticator/",
-    captchaSiteKey: "6LdRVk0qAAAAAGyGQTqJI93DPX7_lx4K4KRpF65C",
+    captchaSiteKey: "6LdOvk0qAAAAAIyDT4QblRGPuCbJ4UyBK7G8D9JT",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseApiUrl : "https://localhost:44316/api/UserAuthenticator/",
-    captchaSiteKey: "6Le2V00qAAAAAJArWrxU6gTomlBLKoSQ_g4dC61m",
+    captchaSiteKey: "6LdOvk0qAAAAAIyDT4QblRGPuCbJ4UyBK7G8D9JT",
 };
