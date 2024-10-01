22d76d471c94f75e15b735997df107fb8c43bebb
AyushSama <aayush0029@gmail.com>
2024-10-01 15:22:36 +0530

    Added More endpoints.ts

--- src/app/endpoints/endpoints.ts
+++ src/app/endpoints/endpoints.ts
@@ -2,4 +2,11 @@ export const endpoint = {
     getUser : "getUser",
     postUser : "postUser",
     getHistory : "getHistory",
+    NewRequest: "/newRequest",
+    PremiumServices: "/premiumServices",
+    Delete : "/delete",
+    Reports : "/reports",
+    ManageAccountDetails : "/manageAccountDetails",
+    LoginPageText: "/loginPageText",
+    NavButtons : "/getNavButtons",
 };
