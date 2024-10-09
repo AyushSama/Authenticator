bd6a53726e8f71890f1852bab63fc1beb67e7727
Prabhat Prasoon <pprasoon@zarca.com>
2024-10-09 17:51:36 +0530

    client list commit

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -9,26 +9,17 @@ import {
 import { ApiService } from '../../service/api.service';
 import { HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Menu } from '../../interfaces/Menu';
-<<<<<<< HEAD
-
-=======
-import { LogindetailsComponent } from '../logindetails/logindetails.component';
-import { Router } from '@angular/router';
->>>>>>> master
 import { SCPLComponent } from '../scpl/scpl.component';
 import { ReportsdataComponent } from '../AccountSummary/reportsdata/reportsdata.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ClientlistComponent } from '../clientlist/clientlist.component';
+import { LogindetailsComponent } from '../logindetails/logindetails.component';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-<<<<<<< HEAD
-  imports: [ReportsdataComponent, CommonModule, SCPLComponent,ClientlistComponent],
-=======
-  imports: [LogindetailsComponent,ReportsdataComponent, CommonModule, SCPLComponent],
->>>>>>> master
+  imports: [ReportsdataComponent, LogindetailsComponent, CommonModule, SCPLComponent,ClientlistComponent],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css',
 })
@@ -37,8 +28,7 @@ export class ReportsComponent implements OnInit {
   @Input() menuId!: number;
   buttonsFound!: Menu[];
   isLt: boolean = false;
-<<<<<<< HEAD
-  genrateData: string = '';
+  generateData: string = '';
 
   constructor(
     private readonly apiService: ApiService,
@@ -47,25 +37,11 @@ export class ReportsComponent implements OnInit {
 
   clientList: boolean = false;
 
-=======
-  generateData: string = '';
-  
-  constructor(private readonly apiService: ApiService, private router : Router) {}
-  
->>>>>>> master
   ngOnInit(): void {
     console.log('report');
     this.getNavButtons(this.menuId);
   }
 
-<<<<<<< HEAD
-  getNavButtons(id: number) {
-    const param = new HttpParams().append('parentId', id);
-    this.apiService.getNavButtons(param).subscribe({
-      next: (res: Menu[]) => {
-        if (res.length != 0) {
-          console.log(res);
-=======
   getNavButtons(id:number){
     const param = new HttpParams().append("parentId" , id);
     this.apiService.getNavButtons(param)
@@ -73,7 +49,6 @@ export class ReportsComponent implements OnInit {
       next : (res : Menu[]) =>{
         if(res.length!=0){
           console.log("Nav ",res);
->>>>>>> master
           this.buttonsFound = res;
         }
       },
@@ -82,20 +57,6 @@ export class ReportsComponent implements OnInit {
       },
     });
   }
-<<<<<<< HEAD
-
-  accountType(button: Menu) {
-    if (button.menuName == 'Account Summary') {
-      this.genrateData = 'showAccountSummaryComponent';
-    } else if (button.menuName == 'Special Case Participants List') {
-      this.genrateData = 'scpl';
-    } 
-
-    else if (button.menuName === 'Client List') {
-      this.genrateData='clientList';
-    } else {
-      this.genrateData = '';
-=======
   
   showLoginDetails(button:any){
     if(this.menuId==4 && button.menuId==7 ){
@@ -103,13 +64,13 @@ export class ReportsComponent implements OnInit {
     }
     else if(this.menuId==4 && button.menuId==8 ){
       this.generateData='LoginDetails';
-    }
-    else if(this.menuId==4 && button.menuId==9 ){
+    }else if (button.menuName === 'Client List') {
+      this.generateData='clientList';
+    }else if(this.menuId==4 && button.menuId==9 ){
       this.generateData='scpl';
     }
     else{
       this.generateData='';
->>>>>>> master
     }
   }
 
