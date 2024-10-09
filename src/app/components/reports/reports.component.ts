571b075f060d9c1f856027f95af9e21f92411644
Prabhat Prasoon <pprasoon@zarca.com>
2024-10-09 17:36:46 +0530

    Client List

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,4 +1,11 @@
-import { Component, ComponentFactoryResolver, Input, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  Input,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { ApiService } from '../../service/api.service';
 import { HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Menu } from '../../interfaces/Menu';
@@ -12,70 +19,61 @@ import { ClientlistComponent } from '../clientlist/clientlist.component';
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [ReportsdataComponent, CommonModule, SCPLComponent],
+  imports: [ReportsdataComponent, CommonModule, SCPLComponent,ClientlistComponent],
   templateUrl: './reports.component.html',
-  styleUrl: './reports.component.css'
+  styleUrl: './reports.component.css',
 })
 export class ReportsComponent implements OnInit {
 
-  @ViewChild('componentDiv', { read: ViewContainerRef, static: true })
-  componentDiv!: ViewContainerRef;
-  
   @Input() menuId!: number;
   buttonsFound!: Menu[];
   isLt: boolean = false;
   genrateData: string = '';
 
+  constructor(
+    private readonly apiService: ApiService,
+    private readonly router: Router,
+  ) {}
 
-  
-  
-  constructor(private readonly apiService: ApiService , private readonly router:Router, private resolver: ComponentFactoryResolver) {}
+  clientList: boolean = false;
 
-  clientList:boolean = false;
-  
   ngOnInit(): void {
-    console.log("report");
-      this.getNavButtons(this.menuId);
+    console.log('report');
+    this.getNavButtons(this.menuId);
   }
 
-  getNavButtons(id:number){
-    const param = new HttpParams().append("parentId" , id);
-    this.apiService.getNavButtons(param)
-    .subscribe({
-      next : (res : Menu[]) =>{
-        if(res.length!=0){
+  getNavButtons(id: number) {
+    const param = new HttpParams().append('parentId', id);
+    this.apiService.getNavButtons(param).subscribe({
+      next: (res: Menu[]) => {
+        if (res.length != 0) {
           console.log(res);
           this.buttonsFound = res;
         }
-      }, 
-      error : (error: HttpErrorResponse)=>{
+      },
+      error: (error: HttpErrorResponse) => {
         console.log(error);
-      }
-    })
+      },
+    });
   }
 
-  accountType(button:Menu ){
-    if(button.menuName=="Account Summary"){
-        this.genrateData='showAccountSummaryComponent';
-    }else if(button.menuName=="Special Case Participants List"){
-      this.genrateData='scpl';
-    }else{
-      this.genrateData='';
+  accountType(button: Menu) {
+    if (button.menuName == 'Account Summary') {
+      this.genrateData = 'showAccountSummaryComponent';
+    } else if (button.menuName == 'Special Case Participants List') {
+      this.genrateData = 'scpl';
+    } 
+
+    else if (button.menuName === 'Client List') {
+      this.genrateData='clientList';
+    } else {
+      this.genrateData = '';
     }
+  }
+
+  navigateToClientList() {
+    this.router.navigateByUrl('clientlist');
   }
 
   
-
-  navigateToClientList(){
-    this.router.navigateByUrl('clientlist')
-  }
-
-  loadComponent(menuName: string) {
-    this.componentDiv.clear(); // Clear previously loaded components
- 
-    if (menuName === 'Client List') {
-      const factory = this.resolver.resolveComponentFactory(ClientlistComponent);
-      this.componentDiv.createComponent(factory);
-    }
-  }
 }
